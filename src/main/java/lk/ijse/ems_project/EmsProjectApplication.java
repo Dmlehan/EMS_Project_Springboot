74b217ddf361525638f74493e6147d9ebab3ab22
Dmlehan <dmlehanamanda@gmail.com>
2025-03-13 08:34:40 +0530

    initial commit

--- src/main/java/lk/ijse/ems_project/EmsProjectApplication.java
+++ src/main/java/lk/ijse/ems_project/EmsProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmsProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmsProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmsProjectApplication.class, args);
+    }
 
 }
